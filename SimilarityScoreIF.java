5d7703b6ff9df4ca71529a7e6af152005f458cb5
parth059 <parthmkpatelwork@gmail.com>
2026-01-31 13:52:47 -0500

    Delete SimilarityScoreIF.java

--- SimilarityScoreIF.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface SimilarityScoreIF {
-    double calculateSimilarity(HouseCriteria criteria, House house);
-    List<House> findTopSimilar(HouseCriteria criteria, List<House> allHouses, int topN);
-}
